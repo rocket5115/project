fb980c38d3e997d88a110295b45484bd400c3c5d
rocket5115 <65498427+rocket5115@users.noreply.github.com>
2021-04-05 16:59:37 +0200

    Create locations.lua

---
+++ locations.lua
@@ -0,0 +1,13 @@
+locations = {
+    {
+        pos={x=-1357.514282266, y=-2852.64941406, z=13.8},
+        tpto={x=1817.5765, y=3650.0444, z=34.1919},
+        submarker= {
+            marker = 29,
+            posz = 15.25,
+        },
+        marker= 23,--[[ get markers here: https://docs.fivem.net/docs/game-references/markers/ ]]
+        scale = 4.0,
+        rgba = {120, 255, 120,155}
+    }
+}
